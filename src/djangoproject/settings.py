b84345e92b332390a9c0625dd446a8c36073995b
mellank <mellank@students.wwu.edu>
2016-02-15 16:08:17 -0800

    Changed the database to postgresql?

--- src/djangoproject/settings.py
+++ src/djangoproject/settings.py
@@ -76,10 +76,23 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+
+        #These settings are specific to postgresql
+        'USER':'mellank',
+        'PASSWORD':'Bentheev1!',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
