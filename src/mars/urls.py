0d4bb13a019b2a5848286d586747d56be4e26516
digren <digren@students.wwu.edu>
2016-04-22 02:46:16 -0700

    Restructured Search into view and form submission, should be working but currently no error checking

--- src/mars/urls.py
+++ src/mars/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^search/$', views.search, name='search'),
     url(r'^contact/sent/$', views.sent, name='sent'),
     url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^$', views.home, name='home'),
 ]
