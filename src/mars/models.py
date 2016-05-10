890541f201f9e08a744c1869d68119974e63e34f
digren <digren@students.wwu.edu>
2016-05-10 04:32:03 -0700

    Fixed the bug, for realzies this time

--- src/mars/models.py
+++ src/mars/models.py
@@ -15,39 +15,38 @@ class SignUp(models.Model):
     return self.emaills
 
 class Sample(models.Model):
-
-	data_id = models.CharField(max_length=15)
-	sample_id = models.CharField(max_length=20)
-	date_accessed = models.DateTimeField(auto_now_add = False, auto_now = False)
-	origin = models.CharField(max_length=50)
-	locality = models.CharField(max_length=50, null=True, blank=True)
-	name = models.CharField(max_length=100)
-	sample_desc = models.CharField(max_length=500, null=True, blank=True)
-	sample_type = models.CharField(max_length=100, null=True, blank=True)
-	sample_class = models.CharField(max_length = 50, null=True, blank=True)
-	grain_size = models.CharField(max_length=100, null=True, blank=True)
-	view_geom = models.CharField(max_length=50, null=True, blank=True)
-	resolution = models.CharField(max_length=50, null=True, blank=True)
-	refl_range = ArrayField(models.FloatField(), size=2, null=True, blank=True)
-	formula = models.CharField(max_length=20, null=True, blank=True)
-	composition = models.CharField(max_length=1000, null=True, blank=True)
-	reflectance = JSONField()
+    data_id = models.CharField(max_length=15)
+    sample_id = models.CharField(max_length=20)
+    date_accessed = models.DateTimeField(auto_now_add = False, auto_now = False)
+    origin = models.CharField(max_length=50)
+    locality = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    sample_desc = models.CharField(max_length=500, null=True, blank=True)
+    sample_type = models.CharField(max_length=100, null=True, blank=True)
+    sample_class = models.CharField(max_length = 50, null=True, blank=True)
+    grain_size = models.CharField(max_length=100, null=True, blank=True)
+    view_geom = models.CharField(max_length=50, null=True, blank=True)
+    resolution = models.CharField(max_length=50, null=True, blank=True)
+    refl_range = ArrayField(models.FloatField(), size=2, null=True, blank=True)
+    formula = models.CharField(max_length=20, null=True, blank=True)
+    composition = models.CharField(max_length=1000, null=True, blank=True)
+    reflectance = JSONField(default = {})
 
     def as_dict(self):
         return {
-          "data_id": self.data_id,
-          "reflectance": self.reflectance,
-          "sample_id" : self.sample_id,
-          "origin" : self.origin,
-          "locality" : self.locality,
-          "name" : self.name,
-          "sample_desc" : self.sample_desc,
-          "sample_type" : self.sample_type,
-          "sample_class" : self.sample_class,
-          "grain_size" : self.grain_size,
-          "view_geom" : self.view_geom,
-          "resolution" : self.resolution,
-          "refl_range" : self.refl_range,
-          "formula" : self.formula,
-          "composition" : self.composition,
+        "data_id": self.data_id,
+        "reflectance": self.reflectance,
+        "sample_id" : self.sample_id,
+        "origin" : self.origin,
+        "locality" : self.locality,
+        "name" : self.name,
+        "sample_desc" : self.sample_desc,
+        "sample_type" : self.sample_type,
+        "sample_class" : self.sample_class,
+        "grain_size" : self.grain_size,
+        "view_geom" : self.view_geom,
+        "resolution" : self.resolution,
+        "refl_range" : self.refl_range,
+        "formula" : self.formula,
+        "composition" : self.composition,
         }
