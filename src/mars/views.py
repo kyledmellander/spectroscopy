59747db48b384e9749ab6b69deef1d7feb54ae94
Neal Digre <digren@spectroscopy.srproj.cs.wwu.edu>
2016-06-20 22:11:38 +0000

    Made fixes name fixes to export

--- src/mars/views.py
+++ src/mars/views.py
@@ -173,7 +173,7 @@ def graph(request):
         # Create the HttpResponse object with the appropriate CSV header
         file = StringIO.StringIO()
         writer = csv.writer(file)
-        names.append(smart_str(s.data_id))
+        names.append(smart_str(s.data_id.strip()))
 
         # Make sure whatever text reader you open this csv file with is set to Unicode (UTF-8)
         writer.writerow([smart_str("Database of Origin"), smart_str(s.origin),])
